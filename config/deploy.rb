bb53488d09379ec5c1ab7ecccb8185f6edb29348
Chris Oliver <excid3@gmail.com>
2015-07-25 19:40:19 -0400

    Restart passenger with touch command

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock '3.4.0'
 set :application, 'deploy_test'
 set :repo_url, 'git@github.com:excid3/deploy_test.git'
 
+set :passenger_restart_with_touch, true
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
